f47f9629c528b41b5b1c7a3babd5ca0f9e1bb148
Jan Maceček <Robotik.888@seznam.cz>
2020-09-18 22:20:32 +0200

    working borrows but unfinished book removal

--- sites/LibrApp-js.js
+++ sites/LibrApp-js.js
@@ -28,17 +28,38 @@ function pushBook(book) {
 }
 
 function borrow() {
-    var title = document.getElementById("titleInput").value;
-    var name = document.getElementById("nameInput").value;
-    var returnDate = document.getElementById("returnDateInput").value;
-    var assumedReturnDate = document.getElementById("assumedReturnDateInput").value;
+    const returndate = new Date().toLocaleDateString();
+    const assumedReturndate = new Date().toLocaleDateString();
+    const title = document.getElementById("titleInput").value;
+    const name = document.getElementById("nameInput").value;
+    returnDate = document.getElementById("returnDateInput").value;
+    assumedReturnDate = document.getElementById("assumedReturnDateInput").value;
     if (name == "" || returnDate == "" || assumedReturnDate == "" || title == "") {
         document.getElementById("paragraph").innerHTML = "Výpůjčku nelze provést - nevyplněná pole.";
     } else {
-        var todayDate = new Date().toLocaleDateString();
+        const todayDate = new Date().toLocaleDateString();
         document.getElementById("paragraph").innerHTML = todayDate + " výpůjčka " + title + " pro " + name + " vypůjčeno do " + returnDate + " předpokládané vrácení " + assumedReturnDate;
         // here will be code for giving information about new borrow to database
+        let borrow = {
+            title: title,
+            name: name,
+            returnDate: returnDate,
+            assumedReturnDate: assumedReturnDate
+        }
+        pushBorrow(borrow);
     }
+}
+
+function pushBorrow(borrow) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            alert("Výpůjčka přidána");
+        }
+    }
+    xmlHttp.open("POST", "http://localhost:8080/borrow/addBorrow", true);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(JSON.stringify(borrow));
 }
 
 function dateFce() {
@@ -71,6 +92,17 @@ function fetchBooks() {
     xmlHttp.send(null);
 }
 
+function fetchBorrows() {
+    let xmlHttp2 = new XMLHttpRequest();
+    xmlHttp2.onreadystatechange = function () {
+        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
+            fillBorrowsTable(JSON.parse(xmlHttp2.responseText));
+        }
+    }
+    xmlHttp2.open("GET", "http://localhost:8080/borrow/listBorrow", true);
+    xmlHttp2.send(null);
+}
+
 function fillTable(books) {
     const table = document.getElementById('book-table');
     for (const book of books) {
@@ -81,5 +113,20 @@ function fillTable(books) {
         cell1.innerHTML = book.name;
         cell2.innerHTML = book.year;
         cell3.innerHTML = book.genre;
+    }
+}
+
+function fillBorrowsTable(borrows) {
+    const table = document.getElementById('borrow-table');
+    for (const borrow of borrows) {
+        const row = table.insertRow(1);
+        const cell1 = row.insertCell();
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+        cell1.innerHTML = borrow.title;
+        cell2.innerHTML = borrow.name;
+        cell3.innerHTML = borrow.returnDate;
+        cell4.innerHTML = borrow.assumedReturnDate;
     }
 }
