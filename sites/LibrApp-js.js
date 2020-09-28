8ed012b1e9f93686a797419ca7240742005755db
Jan Maceček <Robotik.888@seznam.cz>
2020-09-28 17:32:10 +0200

    New functions - deleting borrows and fetch delayed borrows, new site for deleting borrows

--- sites/LibrApp-js.js
+++ sites/LibrApp-js.js
@@ -81,6 +81,7 @@ function dateFce() {
 
 function deleteBook() {
     var title = document.getElementById("titleInput").value;
+    //alert(fetchBooksNames()); - maybe later
     //This may be useful in future
     //var publicationYear = Number(document.getElementById("publicationYearInput").value);
     //var genre = document.getElementById("genreInput").value;
@@ -93,17 +94,62 @@ function deleteBook() {
         // here will be code for giving information about which book should be deleted from database
     }
 }
+
+function deleteBorrow() {
+    let title = document.getElementById("titleInput").value;
+    if (title==="") {
+        document.getElementById("paragraph").innerHTML = "Tato kniha není vypůjčena, nelze ji tudíž odstranit";
+    } else {
+        document.getElementById("paragraph").innerHTML = title + " byla odstraněna z databáze výpůjček";
+        deleteBorrowFromDatabase(title);
+
+        // here will be code for giving information about which book should be deleted from database
+    }
+}
+function deleteBorrowFromDatabase(title) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            alert("Výpůjčka byla odstraněna");
+        }
+    }
+    xmlHttp.open("POST", "http://localhost:8080/borrow/remove", true);
+    xmlHttp.send(title);
+}
 //Working Fetch Names but delay causes problems in implementation of this part
 
+// function WaitFunction(x) {
+//     let xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function () {
+//         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+//             alert ("HI");
+//             books = JSON.parse(xmlHttp.responseText);
+            
+//         }
+//     xmlHttp.open("GET", "http://localhost:8080/book/list/names", true);
+//     xmlHttp.send(null);
+//     setTimeout(function(){ console.log("Waited");}, x);
+//     var booksArray = JSON.stringify(books);
+//     return booksArray;
+// }
+//}
+
+// function myFunction(xmlHttp) {
+//     setTimeout(function(){ var books = JSON.parse(xmlHttp.responseText); var booksArray = JSON.stringify(books); return booksArray;}, 3000);
+    
+//   }
 
 // function fetchBooksNames() {
 //     let xmlHttp = new XMLHttpRequest();
 //     xmlHttp.onreadystatechange = function () {
 //         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-//             var books = JSON.parse(xmlHttp.responseText);
-//             var booksArray = JSON.stringify(books);
-//             return booksArray;
+//             // var books = JSON.parse(xmlHttp.responseText);
+//             // var booksArray = JSON.stringify(books);
+//             // alert(booksArray);
+            
+//             return myFunction(xmlHttp);
 //         }
+        
 //     }
 //     xmlHttp.open("GET", "http://localhost:8080/book/list/names", true);
 //     xmlHttp.send(null);
@@ -144,8 +190,38 @@ function fillTable(books) {
     }
 }
 
+
+
 function fillBorrowsTable(borrows) {
     const table = document.getElementById('borrow-table');
+    for (const borrow of borrows) {
+        const row = table.insertRow(1);
+        const cell1 = row.insertCell();
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+        cell1.innerHTML = borrow.title;
+        cell2.innerHTML = borrow.name;
+        cell3.innerHTML = borrow.returnDate;
+        cell4.innerHTML = borrow.assumedReturnDate;
+    }
+}
+
+
+
+function FetchDelayedBorrows() {
+    let xmlHttp2 = new XMLHttpRequest();
+    xmlHttp2.onreadystatechange = function () {
+        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
+            fillDelayedBorrowsTable(JSON.parse(xmlHttp2.responseText));
+        }
+    }
+    xmlHttp2.open("GET", "http://localhost:8080/borrow/listDelayedBorrow", true);
+    xmlHttp2.send(null);
+}
+
+function fillDelayedBorrowsTable(borrows) {
+    const table = document.getElementById('delayedBorrowTable');
     for (const borrow of borrows) {
         const row = table.insertRow(1);
         const cell1 = row.insertCell();
